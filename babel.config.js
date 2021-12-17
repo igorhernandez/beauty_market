20b04ee30bff8537cb182c4565fd7f53980e58ce
Igor Hernandez <igorhernandez.dev@hotmail.com>
2021-12-16 22:24:38 -0300

    feat: added alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          src: './src',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@navigation': './src/navigation',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
